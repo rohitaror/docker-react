f3db8480691b26ba55a37a1beb3ffad765e50805
ARORA Rohit <rohit.arora2@soprasteria.com>
2019-07-05 10:07:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload Hi 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+                <a>
           Learn React
         </a>
       </header>
+      <p className="app-into">
+        Hi there!
+      </p>
     </div>
   );
 }
